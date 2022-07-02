f535c8c11db0c880a6f682789b6d0c9b976dc2df
Ostrale <enzods@hotmail.fr>
2022-07-02 21:45:16 +0200

    Ajout de commentaires

--- python/mpdp.py
+++ python/mpdp.py
@@ -3,13 +3,16 @@ import random
 import subprocess 
 
 def copy2clip(txt): 
+    "Fonction qui permet d'ajouter un string au presse papier"
     cmd='echo '+txt.strip()+'|clip' 
     return subprocess.check_call(cmd, shell=True)
 
+#On vient ouvrir le .txt avec les mots fr et on les mets dans une liste
 with open("liste_francais.txt", "r") as tf:
     mot = tf.read().split('\n')
 
 def insertf():
+    """Fonction qui permet l'affichage de textes"""
     txt = ''
     nb='0123456789'
     TextChatA.configure(state='normal') #autorise l'écriture dans TextChatA
@@ -29,6 +32,7 @@ def insertf():
     TextChatA.see("end") #permet de mettre la barre de défilement à la fin quand on recoit un message
 
 def generateur():
+    """Générateur de phrase de passe aléatoire sous forme de liste"""
     n = int(saisie.get())
     mdp = []
     position_chiffre = random.randint(0,n-1)
@@ -40,13 +44,15 @@ def generateur():
     mdp = mdp[0:-1]
     return mdp
 
+#Fenetre
 fenetre = tk.Tk()
 fenetre.title("Générateur de phrase de passe")
 
-"""message a afficher dans une zone avec defillement"""
+#message a afficher dans une zone avec defillement
 scrollText = tk.Scrollbar(fenetre)
 TextChatA = tk.Text(fenetre,font="Arial",state='disabled',height = 20, width = 40)
 
+#Zone de texte
 TextChatA.focus_set()
 scrollText.grid(row=1, column=2, rowspan=2, sticky=tk.NS)
 TextChatA.grid(row=1, column=1,rowspan=2)
@@ -56,15 +62,17 @@ TextChatA.tag_config('tiret', foreground="red")
 TextChatA.tag_config('number', foreground="blue")
 TextChatA.tag_config('clipboard', foreground="grey",font=("Courier", 9))
 
+#Bonton de génération de mdp
 bouton=tk.Button(fenetre, text="Générer", command=insertf,borderwidth=0.5)
 bouton.grid(row=5, column=1,columnspan=2)
 
+#nombre de mots utilisé (de base 3)
 label = tk.Label(fenetre, text="Nombres de mot que comporte la phrase de passe :")
 label.grid(row=3, column=1,columnspan=2)
-saisie = tk.StringVar()
+saisie = tk.StringVar() # WARNING : on ne controle jamais qu'on a bien un entier dans cette zone
 ligne_texte = tk.Entry(fenetre, textvariable=saisie, width=8)
 ligne_texte.insert(tk.END,'3')
 ligne_texte.grid(row=4, column=1,columnspan=2)
 
-insertf()
+insertf() #On execute une fois au démarage de la fenetre 
 fenetre.mainloop()
